8dd8eed94daba33ec0f5e7f77863444b8be942be
Oleh Pshyk <pshyk.oleg@gmail.com>
2018-09-13 21:21:44 +0300

    fix getFlags

--- src/getFlags/index.js
+++ src/getFlags/index.js
@@ -6,7 +6,11 @@
  * getFlags(arr); // [1, 2, 3, 4, 5, 6]
  */
 const getFlags = arr => {
-  return arr[0].flags.concat(arr[1].flags);
+  let new = [];
+  for (let item of arr) {
+    new = new.concat(item.flags);
+  }
+  return new;
 };
 
 export default getFlags;
